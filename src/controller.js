b4d9388e55180448d24c002b5305f15434a3aa8d
phamvankhanh <khanhpv@rikkeisoft.com>
2017-07-20 18:23:33 +0700

    Update controller CLI APP

--- src/controller.js
+++ src/controller.js
@@ -1,9 +1,22 @@
 module.exports = {
-  // test: node src/server.js --birthday = 2010
+
   calculate: function () {
-    let b = process.argv[4]
-    let date = new Date().getFullYear().toString()
-    let age = date - b
-    console.log(`"age:" ${age}`)
+    let argv = process.argv
+    let i = 0
+    for (i = 0; i < argv.length; i++) {
+      const currentElement = argv[i]
+      let findBirthday = currentElement.includes('--birthday=')
+      if (findBirthday) {
+        let birthday = currentElement.replace('--birthday=', '')
+        let age = new Date(birthday).getFullYear()
+        if (!Date.parse(age)) {
+          console.log('fail')
+          return false
+        }
+        let dateNow = new Date().getFullYear().toString()
+        let result = dateNow - age
+        console.log(`"age:" ${result}`)
+      }
+    }
   }
 }
